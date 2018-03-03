03003fddf7202713ce4fe665b65cb81f45a3251b
axisimski <33306491+axisimski@users.noreply.github.com>
2018-03-02 18:09:27 -0600

    Added random number generation activity

---
+++ app/src/main/java/axisimski/diceflip/p4.java
@@ -0,0 +1,55 @@
+package axisimski.diceflip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class p4 extends AppCompatActivity {
+
+    EditText low; EditText high; TextView output; Button execute;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p4);
+
+
+        low =(EditText)findViewById(R.id.low);
+        high =(EditText)findViewById(R.id.high);
+        output=(TextView)findViewById(R.id.output);
+        execute=(Button)findViewById(R.id.execute);
+
+
+
+    }
+
+
+    public void execute(View v){
+
+        int l=Integer.getInteger(low.getText().toString());
+        int h=Integer.getInteger(high.getText().toString());
+
+        Random r= new Random();
+
+        int ret=r.nextInt(h)+l;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
